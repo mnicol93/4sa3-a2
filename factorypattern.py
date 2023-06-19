2a2f3b63f2f90cc9fb83dba43d0503971476aeb2
Marc Nicolas Oliva <mnicolas-oliva@myseneca.ca>
2023-06-19 17:16:24 -0400

    Classes created

--- factorypattern.py
+++ factorypattern.py
@@ -23,31 +23,54 @@
 
 # import the open weather data wrapper module
 from pyowm import OWM
-
+from abc import ABC
 
 
 # Implement these classes: Factory, WeatherData, Wind, Humidity, Temperature
 class Factory():
-    def getWeather():
-        return 'Hola'
+    def createData(city, data):
+        if (data == "wind"):
+            return Wind()
 
-class WatherData(Factory):
+
+class WatherData(ABC):
+    location = "unknown"
+
+    @abstractmethod
+    def output():
+        pass
+
 
 class Wind(WeatherData):
+    wind_speed = 0
+
+    def __init__(self, speed):
+        self.wind_speed = speed
+
+    def output(self):
+        print("Wind speed of " + wind_speed + " m/s in " + location)
+
 
 class Humidity(WeatherData):
+    def __init__(self, temperature):
+        self.temperature = temperature
+
 
 class Temperature(WeatherData):
-    
+    def __init__(self, humidity):
+        self.humidity = humidity
+
+
 # Create factory object
 factory = Factory()
+
 owm = OWM('0c26196096af56fb6a9cf51b734a63e2')
 
 # Create a WeatherData object of each type (Wind, Temperature, Humidity) at
 # different locations so we can test our factory's createData instance method
-weatherdata = [factory.createData("Hamilton,ON,CA","wind"), \
-               factory.createData("Toronto,ON,CA","humidity"), \
-               factory.createData("Ottawa,ON,CA","temperature")]
+weatherdata = [factory.createData("Hamilton,ON,CA", "wind"),
+               factory.createData("Toronto,ON,CA", "humidity"),
+               factory.createData("Ottawa,ON,CA", "temperature")]
 
 # Call the output method for each WeatherData object
 for data in weatherdata:
@@ -60,5 +83,3 @@ for data in weatherdata:
 #   Humidity of 35% in Toronto,ON,CA
 #   Temperature of 30.71C in Ottawa,ON,CA
 #
-
-
